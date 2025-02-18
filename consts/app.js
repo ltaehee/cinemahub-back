150f5ee2e55076d9c1a541f6c28fbd69c619afcf
seyoon <se981106@naver.com>
2025-02-18 10:09:43 +0900

    Feat: 배우조회api 추가

--- consts/app.js
+++ consts/app.js
@@ -1,13 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 const PORT = 8080;
 
 const FRONT_URL = process.env.FRONT_URL;
 const MONGODB_URL = process.env.MONGODB_URL;
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+const TMDB_SEARCH_API_KEY = process.env.TMDB_SEARCH_API_KEY;
 
 module.exports = {
   PORT,
   FRONT_URL,
   MONGODB_URL,
   JWT_SECRET_KEY,
+  TMDB_SEARCH_API_KEY,
 };
--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
-const apiController = require('express').Router();
-const loginController = require('./login/login.controller');
-
+const apiController = require("express").Router();
+const loginController = require("./login/login.controller");
+const searchController = require("./search/search.controller");
 /**
  * api/login
  */
-apiController.use('/login', loginController);
+apiController.use("/login", loginController);
+apiController.use("/search", searchController);
 module.exports = apiController;
