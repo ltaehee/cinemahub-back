3045628b618f6798cdd5d925cf10393fa73464e8
이태희 <taehee@itaehuiui-MacBookAir.local>
2025-02-19 10:25:22 +0900

    feat: 마이 페이지 분기처리 완료

--- routes/mypage/mypage.route.js
+++ routes/mypage/mypage.route.js
@@ -6,7 +6,7 @@ const {
 } = require("../../controllers/mypage/mypage.controller");
 
 // 프로필 조회
-router.get("/profile", getProfile);
+router.get("/profile/:nickname", getProfile);
 
 // 프로필 수정
 router.patch("/profile-update", updateProfile);
--- services/mypage/mypage.service.js
+++ services/mypage/mypage.service.js
@@ -1,20 +1,30 @@
 const User = require("../../schemas/user/user.schema");
 
 // 유저 정보 가져오기
-const findUserByEmail = async ({ email }) => {
+const findUserByNickname = async (nickname) => {
+  try {
+    const user = await User.findOne({ nickname }).lean();
+    return user;
+  } catch (e) {
+    throw new Error(e instanceof Error ? e.message : "유저 조회 실패");
+  }
+};
+
+const findUserByEmail = async (email) => {
   try {
     const user = await User.findOne({ email }).lean();
     return user;
   } catch (e) {
-    if (e instanceof Error) throw new Error(e.message);
+    throw new Error(e instanceof Error ? e.message : "유저 조회 실패");
   }
 };
 
 // 유저 프로필 업데이트
 const updateUserProfile = async (userId, updateData) => {
-  return await User.findByIdAndUpdate(userId, updateData, { new: true });
+  return await User.findByIdAndUpdate(userId, updateData, { new: true }).lean();
 };
 module.exports = {
   updateUserProfile,
+  findUserByNickname,
   findUserByEmail,
 };
