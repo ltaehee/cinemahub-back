3fff1b11be480b8952ba7c41542061b67f7a7938
이태희 <taehee@itaehuiui-MacBookAir.local>
2025-04-07 17:25:36 +0900

    SESSION_SERECT_KEY env로 분리

--- consts/app.js
+++ consts/app.js
@@ -1,18 +1,23 @@
-require("dotenv").config();
-const { vaildateEnv } = require("./regex");
+require('dotenv').config();
+const { vaildateEnv } = require('./regex');
 
-const PORT = vaildateEnv("string", process.env.PORT);
+const PORT = vaildateEnv('string', process.env.PORT);
 
-const FRONT_URL = vaildateEnv("url", process.env.FRONT_URL);
+const FRONT_URL = vaildateEnv('url', process.env.FRONT_URL);
 
-const MONGODB_URL = vaildateEnv("mongoose", process.env.MONGODB_URL);
+const MONGODB_URL = vaildateEnv('mongoose', process.env.MONGODB_URL);
 
-const JWT_SECRET_KEY = "jwt_secret_key";
+const JWT_SECRET_KEY = 'jwt_secret_key';
 //vaildateEnv('string', process.env.JWT_SECRET_KEY);
 
-const SESSION_NAME = vaildateEnv("string", process.env.SESSION_NAME);
+const SESSION_NAME = vaildateEnv('string', process.env.SESSION_NAME);
 
-const SESSION_SERECT_KEY = "cinamahub_serect_key";
+// const SESSION_SERECT_KEY = 'cinamahub_serect_key';
+const SESSION_SERECT_KEY = vaildateEnv(
+  'string',
+  process.env.SESSION_SERECT_KEY
+);
+
 // vaildateEnv(
 //   'string',
 //   process.env.SESSION_SERECT_KEY
