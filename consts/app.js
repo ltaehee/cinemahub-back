eb9421ae05437d694209219fea680507962fb758
이태희 <taehee@itaehuiui-MacBookAir.local>
2025-05-27 12:36:51 +0900

    console test

--- consts/app.js
+++ consts/app.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config();
 const { vaildateEnv } = require('./regex');
 
 const PORT = vaildateEnv('string', process.env.PORT);
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-// require('dotenv').config();
+require('dotenv').config();
 
 const app = require('./app');
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Express Running on ${PORT}`));
+console.log('testtest', process.env.NODE_ENV);
